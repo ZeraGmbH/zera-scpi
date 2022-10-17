915c7f53d0d0153cc7b0a5a7af987fd62bbafff8
Andreas Müller <schnitzeltony@gmail.com>
2022-10-17 23:55:04 +0200

    Remove some newlines/whitespace
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/scpi/scpi.cpp
+++ src/scpi/scpi.cpp
@@ -6,9 +6,6 @@
 #include "scpi.h"
 #include "scpi_p.h"
 
-
-
-
 cSCPI::cSCPI(QString interfaceName)
     :d_ptr(new cSCPIPrivate(interfaceName))
 {
@@ -18,7 +15,6 @@ cSCPI::~cSCPI()
 {
     delete d_ptr;
 }
-
 
 void cSCPI::genSCPICmd(const QStringList& parentnodeNames, cSCPIObject *pSCPIObject)
 {
@@ -30,25 +26,21 @@ void cSCPI::delSCPICmds(const QString &cmd)
     d_ptr->delSCPICmds(cmd);
 }
 
-
 void cSCPI::delSCPICmds(cSCPICommand &cmd)
 {
    delSCPICmds(cmd.getCommand());
 }
 
-
 cSCPIObject* cSCPI::getSCPIObject(const QString &input, QString &Param, bool caseSensitive) const
 {
-    return d_ptr->getSCPIObject( input, Param, caseSensitive);
+    return d_ptr->getSCPIObject(input, Param, caseSensitive);
 }
-
 
 cSCPIObject* cSCPI::getSCPIObject(const cSCPICommand &input, bool caseSensitive) const
 {
     QString dummy;
     return this->getSCPIObject(input.getCommand(), dummy, caseSensitive);
 }
-
 
 void cSCPI::exportSCPIModelXML(QString &sxml)
 {
