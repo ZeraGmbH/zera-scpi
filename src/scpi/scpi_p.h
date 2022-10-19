8a1d3eecff1a27169c1a024c4e4f6de18b0229ba
Andreas Müller <schnitzeltony@gmail.com>
2022-10-19 18:14:55 +0200

    cSCPIPrivate: Remove obvious 'documentation'
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/scpi/scpi_p.h
+++ src/scpi/scpi_p.h
@@ -18,29 +18,16 @@ const QString scpimodeldeviceTag =  "DEVICE";
 const QString scpimodelsTag = "MODELS";
 const QString scpinodeAttributeName = "Type";
 
-
-/**
-  @brief
-  The implemention for cSCPI.
-  */
 class cSCPIPrivate {
 public:
-    /**
-      @b Initialise the const variables
-      @param interfaceName the name of the implementation that holds the scpi interface
-      */
     cSCPIPrivate(const QString& interfaceName);
     virtual ~cSCPIPrivate();
-
     /**
       @b Generates 1 scpi command with several parent node names.
       @param[in] parentnodeNames all parent node names.
       @param[in] pSCPIObject the node that holds the object to handle the command.
       */
     void genSCPICmd(const QStringList& parentnodeNames, cSCPIObject* pSCPIObject);
-    /**
-      @b deletes command(s) from the command list
-      */
     void delSCPICmds(const QString& cmd);
     /**
       @b Decodes commands from input, returns null for invalid commands.
@@ -62,29 +49,14 @@ public:
 
 private:
     cSCPINode* createNode(const QString &name, quint8 type, cSCPIObject *scpiObject);
-
-    /**
-      @b deletes all child items
-      */
     void delChildItems(QStandardItem* Item);
-    /**
-      @b deletes itself and all parent items if necessary
-      */
     void delItemAndParents(QStandardItem* Item);
-    /**
-      @b used in the XML parser
-      */
     void appendSCPIRows(QStandardItem* rootItem, QDomDocument &doc, QDomElement &rootElement, quint32 nlevel);
     QStandardItem *findOrCreateChildParentItem(QStandardItem *parentItem, const QStringList& parentnodeNames);
     bool foundItem(QStandardItem *parentItem, cSCPINode** scpiChildItem, QChar* pInput, bool caseSensitive);
 
-    /**
-      @b Name of the device that holds the scpi interface
-      */
+
     QString m_interfaceName;
-    /**
-      @b Holds the parser used for decoding.
-      */
     cParse m_Parser;
 };
 
