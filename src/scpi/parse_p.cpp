c6b5c842d9d2cda9f11ef62206fcd5c605151afe
Andreas Müller <schnitzeltony@gmail.com>
2022-10-19 21:25:38 +0200

    cParsePrivate: Tidy up code a bit
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/scpi/parse_p.cpp
+++ src/scpi/parse_p.cpp
@@ -6,56 +6,32 @@ cParsePrivate::cParsePrivate()
     whitespace = " :;";
 }
 
-
 cParsePrivate::~cParsePrivate()
 {
 }
 
-
-const QString& cParsePrivate::GetKeyword(QChar** s) {
-    QChar tc;
-    bool escape;
-
-    escape = false;
+const QString& cParsePrivate::GetKeyword(QChar** s)
+{
+    bool escape = false;
     keyw ="";
     ignoreWhitespace(s);
-
-    for (;;)
-    {
-       tc = **s;
-       if (tc.isNull()) break; // we are at end of string
+    for (;;) {
+       QChar tc = **s;
+       if (tc.isNull())
+           break; // we are at end of string
        if (!escape && tc == QChar('\\'))
            escape = true;
-       else
-       {
+       else {
            if (escape)
                escape = false;
-           else
-               if ( delimiter.contains(tc,Qt::CaseInsensitive) ) break; // if next char is delimiter, we are ready
-
+           else if (delimiter.contains(tc, Qt::CaseInsensitive))
+               break; // if next char is delimiter, we are ready
            keyw += tc;
        }
-
        (*s)++;
     }
-
-    /*
-    QChar tc=GetChar(s);
-    if (!tc.isNull()) { // whitespace skipped and first character found
-        for (;;) {
-            if ( delimiter.contains(tc,Qt::CaseInsensitive) ) break; // if next char is delimiter, we are ready
-            keyw+=tc;
-            tc=**s; // get next char
-            // if ( delimiter.contains(tc,Qt::CaseInsensitive) ) break; // if next char is delimiter, we are ready
-            if (tc.isNull()) break; // we are at end of string
-            (*s)++; // pointer to next char
-        }
-    }
-    */
-
     return keyw; // keyword without delimiter
 }
-
 
 QChar cParsePrivate::GetChar(QChar** s)
 {
@@ -63,27 +39,27 @@ QChar cParsePrivate::GetChar(QChar** s)
     return **s; // return = 0 or char != whitespace
 }
 
-
-const QString cParsePrivate::SetDelimiter(const QString s) {
+const QString cParsePrivate::SetDelimiter(const QString s)
+{
     QString r = delimiter;
     delimiter = s;
     return(r); // return old delimiter ....
 }
 
-
-const QString cParsePrivate::SetWhiteSpace(const QString s) {
+const QString cParsePrivate::SetWhiteSpace(const QString s)
+{
     QString r = whitespace;
     whitespace = s;
     return(r); // return old whitespace
 }
 
-
 void cParsePrivate::ignoreWhitespace(QChar** s)
 {
-    for (;;)
-    {
-        if ((**s).isNull()) break;
-        if ( !whitespace.contains(**s,Qt::CaseInsensitive) ) break; // if next char is whitespace
+    for (;;) {
+        if ((**s).isNull())
+            break;
+        if ( !whitespace.contains(**s, Qt::CaseInsensitive) )
+            break; // if next char is whitespace
         (*s)++; // pointer to next char
     }
 }
