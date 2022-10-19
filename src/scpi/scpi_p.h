c1a05f22babe3b480a6794c9d3c41ab289744255
Andreas Müller <schnitzeltony@gmail.com>
2022-10-20 00:30:17 +0200

    cSCPIPrivate: Remove obvious 'documentation'
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/scpi/scpi_p.h
+++ src/scpi/scpi_p.h
@@ -19,27 +19,9 @@ class cSCPIPrivate {
 public:
     cSCPIPrivate(const QString& interfaceName);
     virtual ~cSCPIPrivate();
-    /**
-      @b Generates 1 scpi command with several parent node names.
-      @param[in] parentnodeNames all parent node names.
-      @param[in] pSCPIObject the node that holds the object to handle the command.
-      */
     void insertScpiCmd(const QStringList& parentnodeNames, cSCPIObject* pSCPIObject);
     void delSCPICmds(const QString& cmd);
-    /**
-      @b Decodes commands from input, returns null for invalid commands.
-      @param[in] input the received command input
-      @param[out] Param position where to get parameters.
-      */
     cSCPIObject* getSCPIObject(const QString& input, QString& Param, bool caseSensitive = false);
-    /**
-      @b Returns the constructed model.
-      */
-    QStandardItemModel* getSCPIModel();
-    /**
-      @b Generates a xml representation of the model.
-      @param[out] sxml the xml representation as QString
-      */
     void exportSCPIModelXML(QString &sxml);
 
 private:
