0761871f03c92444f577399f4c307f95ccf08bac
Andreas Müller <schnitzeltony@gmail.com>
2022-10-27 21:54:07 +0200

    Finish work on XmlAttribInfo
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- tests/xmlattribinfo.h
+++ tests/xmlattribinfo.h
@@ -9,6 +9,11 @@ class XmlAttribInfo
 {
 public:
     static bool compareElemAttribs(const QDomElement &elem, QMap<QString, QString> attribKeyValues);
+private:
+    static bool attribsEmpty(QMap<QString, QString> attribKeyValues);
+    static bool elemEmpty(const QDomElement &elem);
+    static bool bothEmpty(const QDomElement &elem, QMap<QString, QString> attribKeyValues);
+    static bool compareAll(const QDomElement &elem, QMap<QString, QString> attribKeyValues);
 };
 
 #endif // XMLATTRIBINFO_H
