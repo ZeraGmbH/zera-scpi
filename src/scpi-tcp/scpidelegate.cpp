36ae0144083e5b27027ee299b7fd2ecd3271438b
Andreas Müller <schnitzeltony@gmail.com>
2022-11-04 00:48:57 +0100

    scpidelegate: Use muili-arg arg
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/scpi-tcp/scpidelegate.cpp
+++ src/scpi-tcp/scpidelegate.cpp
@@ -9,7 +9,7 @@
 cSCPIDelegate::cSCPIDelegate(QString cmdParent, QString cmd, quint8 type, cSCPI *scpiInterface, quint16 cmdCode)
     :cSCPIObject(cmd, type), m_nCmdCode(cmdCode)
 {
-    m_sCommand = QString("%1:%2").arg(cmdParent).arg(cmd);
+    m_sCommand = QString("%1:%2").arg(cmdParent, cmd);
     scpiInterface->insertScpiCmd(cmdParent.split(":"), this);
 }
 
