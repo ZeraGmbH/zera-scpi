2a0a1c95b67a168fe4e9987e4c4e3bada4d07174
Andreas Müller <schnitzeltony@gmail.com>
2022-11-10 22:37:32 +0100

    Add XmlElemIterStrategySort tests
    Let's consider them complete
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- tests/test_xmlelemiteratorsort.h
+++ tests/test_xmlelemiteratorsort.h
@@ -7,10 +7,17 @@ class test_xmlelemiteratorsort : public QObject
 {
     Q_OBJECT
 private slots:
+    void noFreakOutOnEmpty();
+
     void noArrayOneNested();
     void noArraySortTwo();
     void noArraySortTwoNested();
     void noArraySortTwoNestedTwo();
+
+    void arraySortTwoNestedTwo();
+    void arraySortTwoNestedTwoMixNonArray();
+    void arraySortTwoNestedTwoMixNonArraySort();
+    void arraySortTwoNestedTwoMixNonArrayObscure();
 };
 
 #endif // TEST_XMLELEMITERATORSORT_H
