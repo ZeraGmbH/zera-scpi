9af3f829bdbc5e11bb64c61d1a3c6f7c5a0dedd8
Andreas Müller <schnitzeltony@gmail.com>
2022-12-23 22:12:33 +0100

    ScpiFullCmdCheckerForTest: Rework interface to keep tests as simple as possible
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- tests/test_scpifullcmdcheckerfortest.cpp
+++ tests/test_scpifullcmdcheckerfortest.cpp
@@ -13,88 +13,77 @@ void test_scpifullcmdcheckerfortest::noMatchOnEmpty()
 void test_scpifullcmdcheckerfortest::checkCmdOnQuery()
 {
     ScpiFullCmdCheckerForTest checker;
-    checker.addCommand(QStringList("SENSE"), "TESTER", SCPI::isQuery);
+    checker.addCommand("SENSE:TESTER", SCPI::isQuery);
     QCOMPARE(checker.matches("sens:test"), false);
 }
 
 void test_scpifullcmdcheckerfortest::matchQueryNoParams()
 {
-    ScpiFullCmdCheckerForTest checker;
-    checker.addCommand(QStringList("SENSE"), "TESTER", SCPI::isQuery);
+    ScpiFullCmdCheckerForTest checker("SENSE:TESTER", SCPI::isQuery);
     QCOMPARE(checker.matches("sens:test?"), true);
 }
 
 void test_scpifullcmdcheckerfortest::testShortParams()
 {
-    ScpiFullCmdCheckerForTest checker;
-    checker.addCommand("SENSE:TESTER", SCPI::isQuery);
+    ScpiFullCmdCheckerForTest checker("SENSE:TESTER", SCPI::isQuery);
     QCOMPARE(checker.matches("sens:test?"), true);
 }
 
 void test_scpifullcmdcheckerfortest::matchQueryOneParam()
 {
-    ScpiFullCmdCheckerForTest checker;
-    checker.addCommand(QStringList("SENSE"), "TESTER", SCPI::isQuery);
+    ScpiFullCmdCheckerForTest checker("SENSE:TESTER", SCPI::isQuery);
     QCOMPARE(checker.matches("sens:test? one"), true);
     QCOMPARE(checker.matches("sens:test? one;"), true);
 }
 
 void test_scpifullcmdcheckerfortest::matchQueryTwoParam()
 {
-    ScpiFullCmdCheckerForTest checker;
-    checker.addCommand(QStringList("SENSE"), "TESTER", SCPI::isQuery);
+    ScpiFullCmdCheckerForTest checker("SENSE:TESTER", SCPI::isQuery);
     QCOMPARE(checker.matches("sens:test? one;two"), true);
     QCOMPARE(checker.matches("sens:test? one;two;"), true);
 }
 
 void test_scpifullcmdcheckerfortest::noMatchQueryTypo()
 {
-    ScpiFullCmdCheckerForTest checker;
-    checker.addCommand(QStringList("SENSE"), "TESTER", SCPI::isQuery);
+    ScpiFullCmdCheckerForTest checker("SENSE:TESTER", SCPI::isQuery);
     QCOMPARE(checker.matches("sens:tes?"), false);
 }
 
 void test_scpifullcmdcheckerfortest::checkQueryOnCmd()
 {
-    ScpiFullCmdCheckerForTest checker;
-    checker.addCommand(QStringList("SENSE"), "TESTER", SCPI::isCmd);
+    ScpiFullCmdCheckerForTest checker("SENSE:TESTER", SCPI::isCmd);
     QCOMPARE(checker.matches("sens:test?"), false);
 }
 
 void test_scpifullcmdcheckerfortest::checkCmdWithParamOnCmdNoParam()
 {
-    ScpiFullCmdCheckerForTest checker;
-    checker.addCommand(QStringList("SENSE"), "TESTER", SCPI::isCmd);
+    ScpiFullCmdCheckerForTest checker("SENSE:TESTER", SCPI::isCmd);
     QCOMPARE(checker.matches("sens:test one;"), false);
     QCOMPARE(checker.matches("sens:test;"), false); // detected as empty param!!!
 }
 
 void test_scpifullcmdcheckerfortest::matchCmdNoParams()
 {
-    ScpiFullCmdCheckerForTest checker;
-    checker.addCommand(QStringList("SENSE"), "TESTER", SCPI::isCmd);
+    ScpiFullCmdCheckerForTest checker("SENSE:TESTER", SCPI::isCmd);
     QCOMPARE(checker.matches("sens:test"), true);
 }
 
 void test_scpifullcmdcheckerfortest::checkCmsWithParamNoParam()
 {
-    ScpiFullCmdCheckerForTest checker;
-    checker.addCommand(QStringList("SENSE"), "TESTER", SCPI::isCmdwP);
+    ScpiFullCmdCheckerForTest checker("SENSE:TESTER", SCPI::isCmdwP);
     QCOMPARE(checker.matches("sens:test"), false);
     QCOMPARE(checker.matches("sens:test one"), false); // not detected as param!!!
 }
 
 void test_scpifullcmdcheckerfortest::matchCmdOneParam()
 {
-    ScpiFullCmdCheckerForTest checker;
-    checker.addCommand(QStringList("SENSE"), "TESTER", SCPI::isCmdwP);
+    ScpiFullCmdCheckerForTest checker("SENSE:TESTER", SCPI::isCmdwP);
     QCOMPARE(checker.matches("sens:test one;"), true);
 }
 
 void test_scpifullcmdcheckerfortest::matchQueryCmdNoParam()
 {
-    ScpiFullCmdCheckerForTest checker;
-    checker.addCommand(QStringList("SENSE"), "TESTER", SCPI::isQuery | SCPI::isCmd);
+    ScpiFullCmdCheckerForTest checker("SENSE:TESTER", SCPI::isQuery | SCPI::isCmd);
     QCOMPARE(checker.matches("sens:test"), true);
     QCOMPARE(checker.matches("sens:test one;"), false);
     QCOMPARE(checker.matches("sens:test?"), true);
@@ -102,10 +91,17 @@ void test_scpifullcmdcheckerfortest::matchQueryCmdNoParam()
 
 void test_scpifullcmdcheckerfortest::matchQueryCmdParam()
 {
-    ScpiFullCmdCheckerForTest checker;
-    checker.addCommand(QStringList("SENSE"), "TESTER", SCPI::isQuery | SCPI::isCmdwP);
+    ScpiFullCmdCheckerForTest checker("SENSE:TESTER", SCPI::isQuery | SCPI::isCmdwP);
     QCOMPARE(checker.matches("sens:test one;"), true);
     QCOMPARE(checker.matches("sens:test one;two;"), true);
     QCOMPARE(checker.matches("sens:test"), false);
     QCOMPARE(checker.matches("sens:test?"), true);
+}
+
+void test_scpifullcmdcheckerfortest::matchMultipleQueryNoParams()
+{
+    ScpiFullCmdCheckerForTest checker("SENSE:FOO", SCPI::isQuery);
+    checker.addCommand("SENSE:BAR", SCPI::isQuery);
+    QCOMPARE(checker.matches("sens:foo?"), true);
+    QCOMPARE(checker.matches("sens:bar?"), true);
 }
