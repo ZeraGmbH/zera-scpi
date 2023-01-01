c1e160a9ee0a9fda2fff00b723453ae1bedafa2d
Andreas Müller <schnitzeltony@gmail.com>
2023-01-01 14:18:37 +0100

    ScpiSingletonFactory: Use std template data types
    We started this because shared pointers are the wrong choice here. Since QHash
    requires copy constuctor which is not available on std::unique_ptr, QHash was
    replaced by std::unordered_map.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- scpi/lib/scpisingletonfactory.cpp
+++ scpi/lib/scpisingletonfactory.cpp
@@ -1,12 +1,12 @@
 #include "scpisingletonfactory.h"
 
-QHash<QString, ScpiSingletonFactory::scpiPtr> ScpiSingletonFactory::m_scpiHash;
+std::unordered_map<QString, ScpiSingletonFactory::scpiPtr> ScpiSingletonFactory::m_scpiHash;
 
 cSCPI* ScpiSingletonFactory::getScpiObj(QString name)
 {
     scpiPtr &entry = m_scpiHash[name];
     if(!entry) {
-        entry = scpiPtr::create(name);
+        entry = std::make_unique<cSCPI>(name);
     }
-    return entry.data();
+    return entry.get();
 }
