e7b18987436b7417f86fd1e6f3a95e462776e277
Asmae Kaouissi <a.kaouissi@zera.de>
2023-07-24 17:11:30 +0200

    main: add the command lines
    Signed-off-by: Asmae Kaouissi <a.kaouissi@zera.de>

--- exec-scpicmds/main.cpp
+++ exec-scpicmds/main.cpp
@@ -5,4 +5,30 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    QCommandLineParser parser;
+    parser.addHelpOption();
+
+    QCommandLineOption filePath("f", "path to the file", "filePath");
+    parser.addOption(filePath);
+
+    QCommandLineOption ipAdr("i", "ip address", "ip address");
+    parser.addOption(ipAdr);
+
+    QCommandLineOption portNumb("p", "port Number", "port Number");
+    parser.addOption(portNumb);
+
+    parser.process(a);
+
+    QString strExecFile = parser.value(filePath);
+    if(strExecFile.isEmpty())
+        parser.showHelp(-1);
+
+    QString ipadr = parser.value(ipAdr);
+    if(ipadr.isEmpty())
+        parser.showHelp(-1);
+
+    QString port = parser.value(portNumb);
+    if(port.isEmpty())
+        parser.showHelp(-1);
 }
