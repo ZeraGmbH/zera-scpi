90163dc48da51994a4c210299fc8f6eabd948a26
Anton Höß <a.hoess@zera.de>
2023-08-02 09:46:02 +0200

    Simplify code to build new SCPI-message incl. added *OPC? for commands
    Signed-off-by: Anton Höß <a.hoess@zera.de>

--- exec-scpicmds/tcphandler.cpp
+++ exec-scpicmds/tcphandler.cpp
@@ -27,24 +27,23 @@ void TcpHandler::sendCommand(QString combinedCmd)
     qInfo("-->  %s ", qPrintable(combinedCmd));
     QStringList cmdsSplitted = combinedCmd.split("|");
     int numAnsw = cmdsSplitted.size();
-    QString combinedCmdWithQueries;
     m_indexAnswers = 0;
     m_cmdIsQuery.clear();
-
+    QStringList cmdWithQueriesSplitted;
     for(auto &cmd : cmdsSplitted) {
+        cmdWithQueriesSplitted.append(cmd);
         if (cmd.trimmed().endsWith("?") ) {
             m_cmdIsQuery.append(true);
-            combinedCmdWithQueries.append(cmd + "|");
         }
         else if (cmd.trimmed().endsWith(";")) {
             m_cmdIsQuery.append(false);
-            cmd.append("|*OPC?|");
-            combinedCmdWithQueries.append(cmd);
+            // Add *OPC? as command-synchronization-barrier for the controller. Its result will get ignored later on.
+            // This is necessary as a timeout gets used for each command and a queue filled
+            // with long running commands erroneously might make working commands running into a timeout.
+            cmdWithQueriesSplitted.append("*OPC?");
         }
     }
-    if(combinedCmdWithQueries.endsWith("|"))
-        combinedCmdWithQueries.remove(combinedCmdWithQueries.length()-1, 1);
-    m_tcpSocket.write(combinedCmdWithQueries.toLocal8Bit() + "\n");
+    m_tcpSocket.write(cmdWithQueriesSplitted.join("|").toLocal8Bit() + "\n");
     m_tcpSocket.waitForBytesWritten();
 
     while(numAnsw > 0) {
@@ -57,7 +56,7 @@ void TcpHandler::disconnectFromHost()
     m_tcpSocket.disconnect();
     m_tcpSocket.close();
     if(m_tcpSocket.state() == QAbstractSocket::UnconnectedState || m_tcpSocket.waitForDisconnected())
-        qInfo("disconnected");
+        qInfo("TCP connection was closed!");
     m_tcpSocket.deleteLater();
 }
 
