4a49ce38a12e0609f19ff561eb43dd7e33b2cce4
Anton Höß <a.hoess@zera.de>
2023-08-22 14:13:29 +0200

    logging: add a style option (bold, ...) for formatting console output
    Signed-off-by: Anton Höß <a.hoess@zera.de>

--- exec-scpicmds/logging.cpp
+++ exec-scpicmds/logging.cpp
@@ -4,12 +4,11 @@
 Logging::Logging(QObject *parent)
     : QObject{parent}
 {
-
 }
 
-QString Logging::colorString(QString strMsg, LoggingColor color)
+QString Logging::colorString(QString strMsg, LoggingColor color, LoggingStyle style)
 {
-    QString colorPre, colorPost;
+    QString colorPre, colorPost, stylePre, stylePost;
 
     if(m_enableGlobalColorLogging)
     {
@@ -32,21 +31,42 @@ QString Logging::colorString(QString strMsg, LoggingColor color)
         case LoggingColor::YELLOW:
             colorPre = QStringLiteral("\033[0;33m");
             colorPost = QStringLiteral("\033[0;0m");
+            break;
+        }
+
+        switch(style)
+        {
+        case LoggingStyle::NONE:
+            break;
+        case LoggingStyle::BOLD:
+            stylePre = QStringLiteral("\033[1m");
+            stylePost = QStringLiteral("\033[0m");
+            break;
+        case LoggingStyle::ITALIC:
+            stylePre = QStringLiteral("\033[1m");
+            stylePost = QStringLiteral("\033[0m");
+            break;
+        case LoggingStyle::UNDERLINE:
+            stylePre = QStringLiteral("\033[4m");
+            stylePost = QStringLiteral("\033[0m");
+            break;
+        case LoggingStyle::STRIKETHROUGH:
+            stylePre = QStringLiteral("\033[9m");
+            stylePost = QStringLiteral("\033[0m");
+            break;
         }
     }
 
-    QString strOut = QStringLiteral("%2%3%4")
-            .arg(colorPre,
-                 strMsg,
-                 colorPost);
+    QString strOut = QStringLiteral("%2%3%4%5%6")
+            .arg(stylePre, colorPre, strMsg, colorPost, stylePost);
 
     return strOut;
 }
 
-void Logging::logMsg(QString strMsg, LoggingColor color)
+void Logging::logMsg(QString strMsg, LoggingColor color, LoggingStyle style)
 {
     QString strOut = QStringLiteral("[%1]: %2")
-            .arg(QDateTime::currentDateTime().toString("HH:mm:ss.zzz"), colorString(strMsg, color));
+            .arg(QDateTime::currentDateTime().toString("HH:mm:ss.zzz"), colorString(strMsg, color, style));
 
     qInfo("%s", qPrintable(strOut));
 }
