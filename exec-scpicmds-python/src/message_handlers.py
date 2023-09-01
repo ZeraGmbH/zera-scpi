56eba276c32fc4d1d630b53945984cf22be0be6a
Anton Höß <a.hoess@zera.de>
2023-09-01 14:31:53 +0200

    TCPHandler and MessageParser: move from main into their own modules
    Signed-off-by: Anton Höß <a.hoess@zera.de>

---
+++ exec-scpicmds-python/src/message_handlers.py
@@ -0,0 +1,64 @@
+from typing import Optional
+from abc import ABC, abstractmethod
+import socket
+
+
+class IMessageHandler(ABC):
+    @abstractmethod
+    def send_message(self, message: str) -> None:
+        pass
+
+    @abstractmethod
+    def receive_response(self) -> Optional[str]:
+        pass
+
+
+class TCPHandler(IMessageHandler):
+    def __init__(self, ip_address: str, port_number: int, receive_timeout: Optional[int] = None) -> None:
+        self._ip_address = ip_address
+        self._port_number = port_number
+        self._receive_timeout = receive_timeout
+        self._receive_buffer = ""
+        self._responses = list()
+        self._connect()
+    
+    def __del__(self) -> None:
+        if self.connected:
+            self._disconnect()
+
+    @property
+    def connected(self) -> bool:
+        return self._socket is not None
+
+    def send_message(self, message: str) -> None:
+        if self._socket is not None:
+            self._socket.send(message.encode())
+
+    def receive_response(self) -> Optional[str]:
+        if len(self._responses) == 0:
+            try:
+                chunk = self._socket.recv(2048).decode()
+            except TimeoutError:
+                return None
+            self._receive_buffer += chunk
+            # Split up complete responses and add them to the result list
+            # There might also be some results from further queries of the current message
+            while "\n" in self._receive_buffer:
+                idx = self._receive_buffer.index("\n")
+                answer = self._receive_buffer[:idx]
+                self._responses.append(answer)
+                self._receive_buffer = self._receive_buffer[idx + 1:]
+        return self._responses.pop(0) if len(self._responses) > 0 else None
+
+    def _connect(self) -> None:
+        self._socket = socket.socket()
+        if self._receive_timeout is not None and self._receive_timeout >= 0:
+            self._socket.settimeout(self._receive_timeout / 1000)
+        try:
+            self._socket.connect((self._ip_address, self._port_number))
+        except:
+            self._socket = None
+
+    def _disconnect(self) -> None:
+        self._socket.close()
+
---
+++ exec-scpicmds-python/src/message_parser.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+
+class MessageParser:
+    @staticmethod
+    def read_messages_from_file(file_path: str) -> Optional[list[str]]:
+        try:
+            with open(file_path, "r") as file:
+                lines = list()
+                while line := file.readline():
+                    line_stipped = line.strip()
+                    if len(line_stipped) > 0 and line_stipped[0] != '#':
+                        lines.append(line.replace("\r", "").replace("\n", ""))
+                return lines
+        except OSError:
+            return None
