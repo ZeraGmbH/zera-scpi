68eecaf9b1d3c7bfc95811a9d1e18586df405b8f
Anton Höß <a.hoess@zera.de>
2023-09-05 19:24:21 +0200

    test_tcp_handler: add new test for the TCPHandler class
    Signed-off-by: Anton Höß <a.hoess@zera.de>

---
+++ exec-scpicmds-python/tests/unittests/test_tcp_handler.py
@@ -0,0 +1,162 @@
+
+import time
+import socket
+import logging
+import threading
+import sys
+sys.path.insert(0, '.')
+from dataclasses import dataclass
+import unittest
+from src.message_handlers import TCPHandler
+
+
+@dataclass
+class ClientData:
+    socket: socket.socket
+    id: int
+
+
+class TcpServer():
+    def __init__(self, ip_address: str, port_number: int, listen_backlog: int = 0):
+        self._ip_address = ip_address
+        self._port_number = port_number
+        self._socket = socket.socket()
+        self._clients: list[ClientData] = list()
+        self._next_client_id = 0
+        self._quit = False
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._socket.setblocking(False)  # Allows for quitting the server while in the accept-loop
+        self._socket.bind((self._ip_address, self._port_number))
+        self._socket.listen(listen_backlog)
+    
+    def __del__(self):
+        self._quit = True
+
+    def run(self, run_in_background: bool = False):
+        if run_in_background:
+            t = threading.Thread(target=self._run)
+            t.start()
+        else:
+            self._run()
+    
+    def _run(self):
+        self.on_server_start()
+        try:
+            self._accept_clients()
+        except Exception as ex:
+            logging.error(ex)
+        finally:
+            self.on_server_stop()
+            for client_data in self._clients:
+                client_data.socket.close()
+            self._socket.close()
+    
+    def quit(self):
+        self._quit = True
+
+    def _accept_clients(self):
+        while not self._quit:
+            try:
+                client_socket, _address = self._socket.accept()
+            except:  # Timeout
+                pass
+            else:
+                self._clients.append(ClientData(client_socket, self._next_client_id))
+                self._next_client_id += 1
+                self.on_client_connection_open(client_socket)
+                threading.Thread(target=self.receive, args=(client_socket,)).start()
+            time.sleep(0.01)  # Throttle loop as accept is non-blocking
+
+    def receive(self, client):
+        while client.fileno() >= 0:  # Loop while socket is open
+            data = client.recv(2048)
+            if data is None or data == b"":
+                break
+            self.on_client_receive_message(client, data)
+        for c, client_data in enumerate(self._clients):
+            if client_data.socket is client:
+                del self._clients[c]
+                break
+        self.on_client_connection_close(client)
+        client.close()
+
+    def broadcast(self, message):
+        for client_data in self._clients:
+            client = client_data.socket
+            if client.fileno() != -1:  # Socket already closed
+                client.send(message)
+
+    def on_server_start(self):
+        pass
+
+    def on_server_stop(self):
+        pass
+
+    def on_client_connection_open(self, client):
+        pass
+
+    def on_client_receive_message(self, client, message):
+        pass
+
+    def on_client_connection_close(self, client):
+        pass
+
+
+class TcpTestServer(TcpServer):
+    def __init__(self, ip_address: str, port_number: int):
+        TcpServer.__init__(self, ip_address, port_number)
+
+    def on_server_start(self):
+        logging.debug("Server started.")
+
+    def on_server_stop(self):
+        logging.debug("Server closed.")
+
+    def on_client_receive_message(self, _client, message):
+        logging.debug(f"Client sent message: {message}")
+        self.broadcast(message)  # Sending message to all clients
+
+    def on_client_connection_open(self, _client):
+        logging.debug("Client connected.")
+
+    def on_client_connection_close(self, _client):
+        logging.debug("Client disconnected.")
+
+
+class TestTCPHandler(unittest.TestCase):
+    def test_connect(self) -> None:
+        ip_address = "localhost"
+        port_number = 16320  # use port different from proper one to avoid conflicts
+        server = TcpTestServer(ip_address, port_number)
+        server.run(run_in_background=True)
+        tcp_handler = TCPHandler(ip_address, port_number, receive_timeout=1000)
+        try:  # Make sure the TCP handler gets disconnected on assertion exception
+            self.assertTrue(tcp_handler.connected)
+        except Exception as e:
+            raise e  # Propagate exception to unittest
+        finally:
+            del tcp_handler
+            server.quit()
+
+    def test_send_receive(self) -> None:
+        ip_address = "localhost"
+        port_number = 16321  # Use different port for each test to allow them being executed in parallel
+        server = TcpTestServer(ip_address, port_number)
+        server.run(run_in_background=True)
+        tcp_handler = TCPHandler(ip_address, port_number, receive_timeout=1000)
+        message = " test_string \n"
+        tcp_handler.send_message(message)
+        response = tcp_handler.receive_response()
+        try:  # Make sure the TCP handler gets disconnected on assertion exception
+            self.assertEqual(message.rstrip('\n'), response)
+        except Exception as e:
+            raise e  # Propagate exception to unittest
+        finally:
+            del tcp_handler
+            server.quit()
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.basicConfig(format="%(message)s")
+    unittest.main()
