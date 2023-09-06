b2565f78a99db6dab9838e172e73ef82f97f14ad
Anton Höß <a.hoess@zera.de>
2023-09-06 15:20:56 +0200

    test_message_parser: fix f-strings
    Signed-off-by: Anton Höß <a.hoess@zera.de>

--- exec-scpicmds-python/tests/unittests/test_message_parser.py
+++ exec-scpicmds-python/tests/unittests/test_message_parser.py
@@ -23,15 +23,15 @@ class TestMessageParser(unittest.TestCase):
                 try:
                     messages = MessageParser.read_messages_from_file(filename)
                 except Exception as e:
-                    self.assertTrue(False, f"Reading temporary test file \"{filename}\" failed with exception \"{e}\".")                
+                    self.assertTrue(False, f"Reading temporary test file \"{filename}\" failed with exception \"{e}\".")
         except Exception as e:
             self.assertTrue(False, f"Writing temporary test file \"{filename}\" failed with exception \"{e}\".")
         self.assertEqual(type(messages), list, "Read invalid number of messages.")
         self.assertEqual(len(messages), 4, "Read invalid number of messages.")
         self.assertEqual(messages[0], lines[0].strip(), f"Message #0 \"{lines[0]}\" is not parsed correctly.")
         self.assertEqual(messages[1], lines[1], f"Message #1 \"{(lines[1][:20] + '...') if len(lines[1]) < 20 else lines[1]}\" is not parsed correctly.")
-        self.assertEqual(messages[2], lines[3].strip(), "Message #3 \"{lines[3]}\" is not parsed correctly.")
-        self.assertEqual(messages[3], lines[4].strip(), "Message #4 \"{lines[4]}\" is not parsed correctly.")
+        self.assertEqual(messages[2], lines[3].strip(), f"Message #3 \"{lines[3]}\" is not parsed correctly.")
+        self.assertEqual(messages[3], lines[4].strip(), f"Message #4 \"{lines[4]}\" is not parsed correctly.")
 
 
 if __name__ == "__main__":
