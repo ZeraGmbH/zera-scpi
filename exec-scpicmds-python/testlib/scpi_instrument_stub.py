881dbdb52a6c838711e362253f725a5f17f0fe3a
Anton Höß <a.hoess@zera.de>
2023-09-07 15:17:45 +0200

    test_exec_scpicmds: add integration test for whole program
    More details:
    * Add new integration test for the whole program on
    command line using the new class ScpiInstrumentStub
    
    Signed-off-by: Anton Höß <a.hoess@zera.de>

---
+++ exec-scpicmds-python/testlib/scpi_instrument_stub.py
@@ -0,0 +1,31 @@
+import socket
+import random
+from testlib.tcp_server_helper import VerboseTcpServer
+
+
+# This class only handles a subset of the commands and of the syntax (e.g. queries without parameters) and returns pseudo responses
+class ScpiInstrumentStub(VerboseTcpServer):
+    def __init__(self, ip_address: str, port_number: int) -> None:
+        VerboseTcpServer.__init__(self, ip_address, port_number)
+
+    def on_client_receive_message(self, client: socket.socket, message: str) -> None:
+        super().on_client_receive_message(client, message)
+        send = lambda response: client.send(response.encode())
+        commands = [command.strip().upper() for command in message.split("|")]
+        for command in commands:
+            if command.endswith("?"):
+                keywords = command.replace("?", "").split(":")
+                match keywords:
+                    case ["*IDN"]:
+                        send("ZERA SCPI Instrument Stub version 0.0.1\n")
+                    case ["*STB"]:
+                        send("+0\n")
+                    case ["MEASURE", ("DFT1" | "FFT1"), ("UL1" | "UL2" | "UL3" | "UL1-UL2" | "UL3-UL2" | "UL3-UL1" | "UAUX" | "IL1" | "IL2" | "IL3" | "IL1-IL2" | "IL3-IL2" | "IL3-IL1" | "IAUX")]:
+                        send(",".join([str(random.random()) for i in range((20 + 1) * 2)]) + "\n")
+                    case _:
+                        send("ResponseToUnknownQuery\n")
+
+
+if __name__ == "__main__":
+    instrument = ScpiInstrumentStub("localhost", 1234)
+    instrument.run()
---
+++ exec-scpicmds-python/tests/integrationtests/test_exec_scpicmds.py
@@ -0,0 +1,56 @@
+import logging
+import os
+import sys
+sys.path.insert(0, '.')
+import unittest
+from src.message_parser import MessageParser
+from testlib.message_parser_helper import FileWriterHelper
+from testlib.tcp_server_helper import PortNumberGenerator
+from testlib.scpi_instrument_stub import ScpiInstrumentStub
+from src.exec_scpicmds import ExecScpiCmdsProgram
+
+
+def setUpModule():
+    start_port = 16520
+    port_cnt = 100
+    PortNumberGenerator.set_port_range(start_port, start_port + port_cnt - 1)
+
+
+class TestExecScpiCmds(unittest.TestCase):
+    def test_program_exit_code_with_basic_commandline_arguments(self) -> None:
+        # Prepare message file
+        filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "example_file_for_test_program_exit_code_with_basic_commandline_arguments.txt")
+        lines = list()
+        lines.append(" \t*IDN?| *IDN? \t ")
+        lines.append("*STB?")
+        lines.append("MEASURE:FFT1:UL1?")
+        lines.append("TEST")
+        lines.append("TEST:TEST2")
+        file_writer = FileWriterHelper(filename, lines)
+        file_writer.write_file()
+        # Run program to send messages over TCP/IP and read reponses
+        ip_address = "localhost"
+        port_number = PortNumberGenerator.get_next_port_number()  # Use different port for each test to allow them being executed in parallel
+        logging.debug("Starting instrument...")
+        instrument = ScpiInstrumentStub(ip_address, port_number)
+        instrument.run(run_in_background=True)
+        try:  # Make sure to cleanup on assertion exception
+            sys.argv[1:] = ["-i", ip_address, "-p", str(port_number), "-f", filename]
+            logging.debug("Running program...")
+            program = ExecScpiCmdsProgram()
+            program.run()
+        except SystemExit as e:
+            self.assertEqual(e.code, 0, f"Program returned with exit code {e.code}.")
+            # TODO modify program (add command line parameter to exit on errors like timeout?) to exit (with different codes) on errors
+        except Exception as e:
+            raise e  # Propagate exception to unittest
+        finally:
+            logging.debug("Cleanup...")
+            instrument.quit()
+            file_writer.remove_file()
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.basicConfig(format="%(message)s")
+    unittest.main()
