90ecfa8c72d50e4dbd958067aea2dabb33818074
Anton Höß <a.hoess@zera.de>
2023-09-07 14:22:44 +0200

    message_parser_helper: make FileWriterHelper also usable outside context manager
    Signed-off-by: Anton Höß <a.hoess@zera.de>

--- exec-scpicmds-python/testlib/message_parser_helper.py
+++ exec-scpicmds-python/testlib/message_parser_helper.py
@@ -11,17 +11,20 @@ class FileWriterHelper:
         self._line_terminator = line_terminator
 
     def __enter__(self) -> None:
-        self._remove_file()
+        self.remove_file()
+        self.write_file()
+        return None  # There is no useful object to return
+
+    def __exit__(self, _exc_type: Optional[Type[BaseException]], _exc_val: Optional[BaseException], _traceback: Optional[types.TracebackType]) -> None:
+        self.remove_file()
+    
+    def write_file(self) -> None:
         file = open(self._filename, "a")
         for line in self._lines:
             file.write(line + self._line_terminator)
         file.close()
-        return None  # There is no useful object to return
 
-    def __exit__(self, _exc_type: Optional[Type[BaseException]], _exc_val: Optional[BaseException], _traceback: Optional[types.TracebackType]) -> None:
-        self._remove_file()
-    
-    def _remove_file(self) -> None:
+    def remove_file(self) -> None:
         if os.path.exists(self._filename):
             logging.debug(f"Removing file \"{self._filename}\".")
             os.remove(self._filename)
