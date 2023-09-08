453a82e009d8535af3818f21293773d038a8035f
Anton Höß <a.hoess@zera.de>
2023-09-08 14:24:08 +0200

    Prepare some classes and functions for SCPI message parsing and add tests
    More details:
    * Classes: CommandType, CommandData, MessageData
    * Functions: get_command_type_from_string(), check_message_is_valid(), get_message_data_from_string()
    
    Signed-off-by: Anton Höß <a.hoess@zera.de>

--- exec-scpicmds-python/tests/unittests/test_message_parser.py
+++ exec-scpicmds-python/tests/unittests/test_message_parser.py
@@ -3,7 +3,7 @@ import logging
 import os
 import sys
 sys.path.insert(0, '.')
-from src.message_parser import MessageParser
+from src.message_parser import MessageParser, CommandType, CommandData, MessageData
 from testlib.message_parser_helper import FileWriterHelper
 
 
@@ -58,6 +58,43 @@ class TestMessageParser(unittest.TestCase):
         for idx_read, idx_ori in enumerate(lines_with_real_messages):
             self.assertEqual((messages[idx_read], line_numbers[idx_read]), (lines[idx_ori].strip(), idx_ori), f"Message \"{lines[idx_ori].strip()}\" and its line number #{idx_ori} is not parsed correctly.")
 
+    def test_get_command_type_from_string(self) -> None:
+        self.assertEqual(MessageParser.get_command_type_from_string("TEST?"), CommandType.QUERY, f"Wrong command type returned.")
+        self.assertEqual(MessageParser.get_command_type_from_string("TEST"), CommandType.COMMAND, f"Wrong command type returned.")
+        self.assertEqual(MessageParser.get_command_type_from_string("TEST myparam;"), CommandType.COMMAND, f"Wrong command type returned.")
+        self.assertEqual(MessageParser.get_command_type_from_string("  "), CommandType.EMPTY, f"Wrong command type returned.")
+
+    def test_check_message_is_valid(self) -> None:
+        self.assertCountEqual(CommandType, [CommandType.UNKNOWN, CommandType.COMMAND, CommandType.QUERY, CommandType.EMPTY], f"Not all enum values are tested.")
+        for command_types in [[CommandType.COMMAND], [CommandType.QUERY], [CommandType.COMMAND, CommandType.QUERY]]:
+            message_data = MessageData()
+            message_data.commands=[CommandData("", "", command_type) for command_type in command_types]
+            self.assertTrue(MessageParser.check_message_is_valid(message_data), f"Validity-check of message data incorrectly returned a negative result.")
+        for command_types in [[], [CommandType.EMPTY], [CommandType.UNKNOWN], [CommandType.COMMAND, CommandType.EMPTY], [CommandType.QUERY, CommandType.UNKNOWN]]:
+            message_data = MessageData()
+            message_data.commands=[CommandData("", "", command_type) for command_type in command_types]
+            self.assertFalse(MessageParser.check_message_is_valid(message_data), f"Validity-check of message data incorrectly returned a negative result.")
+
+    def test_get_message_data_from_string(self) -> None:
+        message = " \t *IDN? | TEST || |  TEST \tmyparam;| "
+        line_number = 42
+        message_data = MessageParser.get_message_data_from_string(message, line_number)
+        self.assertFalse(message_data.is_valid, f"Message data: is_valid-property parsed incorrectly.")
+        self.assertEqual(message_data.file_line_number, line_number, f"Message data: file_line_number-property parsed incorrectly.")
+        self.assertEqual(message_data.original_message, message, f"Message data: original_message-property parsed incorrectly.")
+        self.assertEqual(message_data.command_count_string_width, len(str(len(message_data.commands))), f"Message data: command_count_string_width-property parsed incorrectly.")
+        self.assertEqual(len(message_data.commands), 6, "Invalid number of commands in message.")
+        expected_results = [[" \t *IDN? ", "*IDN?", CommandType.QUERY, 0],
+                            [" TEST ", "TEST", CommandType.COMMAND, 10],
+                            ["", "", CommandType.EMPTY, 17],
+                            [" ", "", CommandType.EMPTY, 18],
+                            ["  TEST \tmyparam;", "TEST \tmyparam;", CommandType.COMMAND, 20],
+                            [" ", "", CommandType.EMPTY, 37]]
+        for r, result in enumerate(expected_results):
+            self.assertEqual(message_data.commands[r].command, result[0], f"Command data {r}: command-property parsed incorrectly.")
+            self.assertEqual(message_data.commands[r].command_trimmed, result[1], f"Command data {r}: command_trimmed-property parsed incorrectly.")
+            self.assertEqual(message_data.commands[r].command_type, result[2], f"Command data {r}: command_type-property parsed incorrectly.")
+            self.assertEqual(message_data.commands[r].position_in_message, result[3], f"Command data {r}: position_in_message-property parsed incorrectly.")
 
 
 if __name__ == "__main__":
