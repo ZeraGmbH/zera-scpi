6882a42c892153f6153a30b2ec155c63bab334ef
Anton Höß <a.hoess@zera.de>
2023-09-08 12:43:41 +0200

    message_parser: add MessageParser.read_messages_with_lines_from_file() and its test
    Signed-off-by: Anton Höß <a.hoess@zera.de>

--- exec-scpicmds-python/src/message_parser.py
+++ exec-scpicmds-python/src/message_parser.py
@@ -1,17 +1,30 @@
-from typing import Optional, List
+from typing import Optional, List, Tuple
 
 
 class MessageParser:
     @staticmethod
     def read_messages_from_file(file_path: str) -> Optional[List[str]]:
+        result = MessageParser.read_messages_with_lines_from_file(file_path)
+        if result is not None:
+            lines, _ = result
+            return lines
+        else:
+            return None
+
+    @staticmethod
+    def read_messages_with_lines_from_file(file_path: str) -> Optional[Tuple[List[str], List[int]]]:
         try:
             with open(file_path, "r") as file:
                 lines = list()
+                line_numbers = list()
+                current_line_number = 0
                 while line := file.readline():
                     line_stipped = line.strip()
                     if len(line_stipped) > 0 and line_stipped[0] != '#':
                         lines.append(line.replace("\r", "").replace("\n", "").strip())
-                return lines
+                        line_numbers.append(current_line_number)
+                    current_line_number += 1
+                return lines, line_numbers
         except OSError:
             return None
 
--- exec-scpicmds-python/tests/unittests/test_message_parser.py
+++ exec-scpicmds-python/tests/unittests/test_message_parser.py
@@ -27,12 +27,37 @@ class TestMessageParser(unittest.TestCase):
                     self.assertTrue(False, f"Reading temporary test file \"{filename}\" failed with exception \"{e}\".")
         except Exception as e:
             self.assertTrue(False, f"Writing temporary test file \"{filename}\" failed with exception \"{e}\".")
-        self.assertEqual(type(messages), list, "Read invalid number of messages.")
+        self.assertEqual(type(messages), list, "Return value is not a list.")
         self.assertEqual(len(messages), 4, "Read invalid number of messages.")
         self.assertEqual(messages[0], lines[0].strip(), f"Message #0 \"{lines[0]}\" is not parsed correctly.")
         self.assertEqual(messages[1], lines[1], f"Message #1 \"{(lines[1][:20] + '...') if len(lines[1]) < 20 else lines[1]}\" is not parsed correctly.")
         self.assertEqual(messages[2], lines[3].strip(), f"Message #3 \"{lines[3]}\" is not parsed correctly.")
         self.assertEqual(messages[3], lines[4].strip(), f"Message #4 \"{lines[4]}\" is not parsed correctly.")
+
+    def test_read_messages_with_lines_from_file(self) -> None:
+        filename = MessageParser.get_filename_from_class_and_method(self, self.test_read_messages_with_lines_from_file) + ".txt"
+        filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+        lines = list()
+        lines.append("# My comment")
+        lines.append("\t*IDN? |IDN?  ")
+        lines.append("\t")
+        lines.append("*STB?")
+        messages_and_lines = None
+        try:
+            with FileWriterHelper(filename, lines):
+                try:
+                    messages_and_lines = MessageParser.read_messages_with_lines_from_file(filename)
+                except Exception as e:
+                    self.assertTrue(False, f"Reading temporary test file \"{filename}\" failed with exception \"{e}\".")
+        except Exception as e:
+            self.assertTrue(False, f"Writing temporary test file \"{filename}\" failed with exception \"{e}\".")
+        self.assertNotEqual(messages_and_lines, None, "The function returned None (due to an exception).")
+        messages, line_numbers = messages_and_lines
+        self.assertEqual(len(messages), 2, "Read invalid number of messages/lines.")
+        lines_with_real_messages = [1, 3]
+        for idx_read, idx_ori in enumerate(lines_with_real_messages):
+            self.assertEqual((messages[idx_read], line_numbers[idx_read]), (lines[idx_ori].strip(), idx_ori), f"Message \"{lines[idx_ori].strip()}\" and its line number #{idx_ori} is not parsed correctly.")
+
 
 
 if __name__ == "__main__":
