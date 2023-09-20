e13e6949b081b3509d9f79773065093a09a10dd4
Anton Höß <a.hoess@zera.de>
2023-09-20 09:50:02 +0200

    test_exec_scpi_cmds_args_parser: improve test coverage
    Signed-off-by: Anton Höß <a.hoess@zera.de>

--- exec-scpicmds-python/tests/unittests/test_exec_scpi_cmds_args_parser.py
+++ exec-scpicmds-python/tests/unittests/test_exec_scpi_cmds_args_parser.py
@@ -11,17 +11,34 @@ class TestExecScpiCmdsArgsParser(unittest.TestCase):
         i = "localhost"      # ip_address
         p = 6320             # port_number
         f = "scpi_cmds.txt"  # filename
+        # Note: shlex.split() cannot properly split short options with values that are not separated by space(s), e.g. -ilocalhost
         argv = shlex.split(f"-i {i} -p {p} -f 'space test {f}'")
         sys.argv[1:] = argv
         self.assertEqual(ExecScpiCmdsArgsParser.parse(), ExecScpiCmdsArgsParser.parse(argv), f"Parsing args with sys.argv and as function argument are not equal.")
         self.assertNotEqual(ExecScpiCmdsArgsParser.parse(), None, f"Parsing args with sys.argv {sys.argv[1:]} failed.")
         valid_cmd_lines = list()
         valid_cmd_lines.append(f"-i {i} -p {p} -f '{f}'")
+        valid_cmd_lines.append(f"-i {i} -p {p} -f '{f}' -r 0")
+        valid_cmd_lines.append(f"-i {i} -p {p} -f '{f}' -r 1")
+        valid_cmd_lines.append(f"-i {i} -p {p} -f '{f}' -r 1000")
+        valid_cmd_lines.append(f"-i {i} -p {p} -f '{f}' --number-of-repetitions 1000")
+        valid_cmd_lines.append(f"-i {i} -p {p} -f '{f}' --sync_cmds_with_opc")
+        valid_cmd_lines.append(f"-i {i} -p {p} -f '{f}' --enable-formatted-output")
         for valid_cmd_line in valid_cmd_lines:
             self.assertNotEqual(ExecScpiCmdsArgsParser.parse(shlex.split(valid_cmd_line)), None, f"Parsing args \"{valid_cmd_line}\" failed.")
-        
+
         invalid_cmd_lines = list()
+        invalid_cmd_lines.append(f"-i {i} -p {0}")
+        invalid_cmd_lines.append(f"-i {i} -f '{f}'")
+        invalid_cmd_lines.append(f"-p {0} -f '{f}'")
         invalid_cmd_lines.append(f"-i {i} -p {0} -f '{f}'")
+        invalid_cmd_lines.append(f"-i {i} -p {p} -f '{f}' -t 0")
+        invalid_cmd_lines.append(f"-i {i} -p {p} -f '{f}' -t a")
+        invalid_cmd_lines.append(f"-i {i} -p {p} -f '{f}' -r a")
+        invalid_cmd_lines.append(f"-i {i} -p {p} -f '{f}' --sync_cmds_with_opc 0")
+        invalid_cmd_lines.append(f"-i {i} -p {p} -f '{f}' --sync_cmds_with_opc 1")
+        invalid_cmd_lines.append(f"-i {i} -p {p} -f '{f}' --enable-formatted-output 0")
+        invalid_cmd_lines.append(f"-i {i} -p {p} -f '{f}' --enable-formatted-output 1")
         for invalid_cmd_line in invalid_cmd_lines:
             self.assertEqual(ExecScpiCmdsArgsParser.parse(shlex.split(invalid_cmd_line)), None, f"Parsing args \"{invalid_cmd_line}\" unexpectedly succeeded.")
 
