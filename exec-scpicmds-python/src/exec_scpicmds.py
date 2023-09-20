ad43286d102b95e3f4cf7fc89bdfccc7178b99d2
Anton Höß <a.hoess@zera.de>
2023-09-20 09:12:05 +0200

    exec_scpicmds: move argparse to new class ExecScpiCmdsArgsParser; add tests
    Signed-off-by: Anton Höß <a.hoess@zera.de>

--- exec-scpicmds-python/src/exec_scpicmds.py
+++ exec-scpicmds-python/src/exec_scpicmds.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from typing import List
+from typing import List, Optional
 import os
 import sys
 sys.path.insert(0, '.')
@@ -11,52 +11,58 @@ from src.message_parser import MessageParser, CommandType, MessageData
 from src.message_handlers import TCPHandler
 
 
-class ExecScpiCmdsProgram:
-    @staticmethod
-    def run() -> int:
-        Logging.setup(log_level=logging.DEBUG)
+class ExecScpiCmdsArgsParser:
+    def _check_positive_integer(value: str, excl_zero: bool=False) -> int:
+        int_value = int(value)
+        if (excl_zero and int_value <= 0) or (not excl_zero and int_value < 0):
+            raise argparse.ArgumentTypeError(f"{value} is not a positive int value!")
+        return int_value
 
-        def check_positive_integer(value: str, excl_zero: bool = False) -> int:
-            int_value = int(value)
-            if (excl_zero and int_value <= 0) or (not excl_zero and int_value < 0):
-                raise argparse.ArgumentTypeError(f"{value} is not a positive int value!")
-            return int_value
-
-        def check_port_number(value: str) -> int:
-            int_value = int(value)
-            if not 0 < int_value <= 65535:
-                raise argparse.ArgumentTypeError(f"{value} is not a valid port number!")
-            return int_value
+    def _check_port_number(value: str) -> int:
+        int_value = int(value)
+        if not 0 < int_value <= 65535:
+            raise argparse.ArgumentTypeError(f"{value} is not a valid port number!")
+        return int_value
     
+    @staticmethod
+    def parse(args: Optional[List[str]]=None) -> Optional[argparse.Namespace]:
         parser = argparse.ArgumentParser(description="Execute SCPI commands and optionally check results.")
         parser.add_argument("-f", "--input-file", required=True,
                             help="Path to file containing SCPI commands.")
         parser.add_argument("-i", "--ip-address", required=True,
                             help="IP-address of instrument.")
-        parser.add_argument("-p", "--port-number", required=True, type=check_port_number,
+        parser.add_argument("-p", "--port-number", required=True, type=ExecScpiCmdsArgsParser._check_port_number,
                             help="Port number of instrument.")
-        parser.add_argument("-t", "--receive-timeout", type=lambda x: check_positive_integer(x, excl_zero=True), default=3000,
+        parser.add_argument("-t", "--receive-timeout", type=lambda x: ExecScpiCmdsArgsParser._check_positive_integer(x, excl_zero=True), default=3000,
                             help="Receive timeout [ms] of TCP/IP-connection to instrument.")
         parser.add_argument("--enable-formatted-output", action="store_true", default=False,
                             help="Enable output formatted with colors and styles.")
-        parser.add_argument("-r", "--number-of-repetitions", type=lambda x: check_positive_integer(x, excl_zero=False), default=1,
+        parser.add_argument("-r", "--number-of-repetitions", type=lambda x: ExecScpiCmdsArgsParser._check_positive_integer(x, excl_zero=False), default=1,
                             help="Number of repetitions sending whole SCPI command file.")
         parser.add_argument("--sync_cmds_with_opc", action="store_true", default=False,
                             help="Add an \"|*OPC?\" query after each command to synchronize it. "\
                                 "Also expects more responses, which will be hidden from the user. Only that way a timeout can get detected. "\
                                 "\033[0;31mAttention:\033[0m this feature only makes sense when queueing of SCPI commands on the target instrument is activated.")
-
         try:
-            args = parser.parse_args()
+            if args is None:
+                parser_args = parser.parse_args()
+            else:
+                parser_args = parser.parse_args(args)
+            return parser_args
         except SystemExit:
+            return None
+
+class ExecScpiCmdsProgram:
+    @staticmethod
+    def run() -> int:
+        Logging.setup(log_level=logging.DEBUG)
+        args = ExecScpiCmdsArgsParser.parse()
+        if args is None:
             return 1
-
         Logging.enable_formatted_output(args.enable_formatted_output)
-
         if not os.path.exists(args.input_file):
             Logging.log_debug_msg(f"File \"{args.input_file}\" does not exist!", LoggingColor.RED)
             return 2
-
         if (messages_and_lines := MessageParser.read_messages_with_lines_from_file(args.input_file)) is None:
             Logging.log_debug_msg(f"Reading file \"{args.input_file}\" failed!", LoggingColor.RED)
             return 3
