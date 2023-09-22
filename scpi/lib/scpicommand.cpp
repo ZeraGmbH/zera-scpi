88cf99615da6a57362d2ef1bdca03e68ba775fa5
Andreas Müller <schnitzeltony@gmail.com>
2023-09-22 20:04:08 +0200

    Remove cSCPICommandPrivate
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- scpi/lib/scpicommand.cpp
+++ scpi/lib/scpicommand.cpp
@@ -1,101 +1,121 @@
 #include "scpicommand.h"
-#include "scpicommand_p.h"
-
+#include "parse.h"
 
 cSCPICommand::cSCPICommand()
-    :d_ptr(new cSCPICommandPrivate)
 {
 }
 
-
-cSCPICommand::cSCPICommand(const QString& str)
-    :d_ptr(new cSCPICommandPrivate(str))
+cSCPICommand::cSCPICommand(const QString& str) :
+    m_sCommand(str)
 {
+    setCmdParamList();
 }
 
-cSCPICommand::cSCPICommand(const cSCPICommand &other)
-  :d_ptr(new cSCPICommandPrivate(other.getCommand()))
+cSCPICommand::cSCPICommand(const cSCPICommand &other) :
+    m_sCommand(other.getCommand())
 {
+    setCmdParamList();
 }
-
-
-cSCPICommand::~cSCPICommand()
-{
-    delete d_ptr;
-}
-
 
 cSCPICommand& cSCPICommand::operator = (const QString& str)
 {
-    *d_ptr = str;
+    m_sCommand = str;
+    setCmdParamList();
     return *this;
 }
 
-
 QTextStream& operator >> (QTextStream& ts, cSCPICommand& cmd)
 {
-    return ts >> *(cmd.d_ptr);
+    cmd.m_sCommand = "";
+    while (!ts.atEnd()) // we read the whole stream for 1 command
+    {
+        QString s;
+        ts >> s;
+        s += " "; // we append blank character because stream removes them
+        cmd.m_sCommand += s;
+    }
+    cmd.setCmdParamList();
+    return ts;
 }
-
 
 const QString cSCPICommand::getCommand() const
 {
-    return d_ptr->m_sCommand;
+    return m_sCommand;
 }
-
 
 const QString cSCPICommand::getCommandStr() const
 {
-    return d_ptr->m_sCommandStr;
+    return m_sCommandStr;
 }
-
 
 quint32 cSCPICommand::getParamCount() const
 {
-    return d_ptr->m_sParamList.count();
+    return m_sParamList.count();
 }
-
 
 QString cSCPICommand::getParam(qint32 pos) const
 {
-    QString s;
-    if (pos < d_ptr->m_sParamList.count())
-        return d_ptr->m_sParamList[pos];
+    if (pos < m_sParamList.count())
+        return m_sParamList[pos];
     else
-        return s;
+        return QString();
 }
-
 
 QString cSCPICommand::getParam() const
 {
-    QString s, cmd;
-
-    s = d_ptr->m_sCommand;
-    cmd = d_ptr->m_sCommandStr;
-
+    QString s = m_sCommand;
+    QString cmd = m_sCommandStr;
     s = s.right(s.length() - cmd.length());
-
     return s;
 }
 
 const QStringList &cSCPICommand::getParamList() const
 {
-  return d_ptr->m_sParamList;
+  return m_sParamList;
 }
 
 bool cSCPICommand::isQuery() const
 {
-    return d_ptr->isQuery();
+    return ( (m_sCommandStr.contains("?")) && (m_sParamList.count() == 0));
 }
-
 
 bool cSCPICommand::isQuery(quint8 anzParameter) const
 {
-    return d_ptr->isQuery(anzParameter);
+    return ( (m_sCommandStr.contains("?")) && (m_sParamList.count() == anzParameter));
 }
-
 
 bool cSCPICommand::isCommand(quint8 anzParameter) const
 {
-    return d_ptr->isCommand(anzParameter);
+    return ( (!m_sCommandStr.contains("?")) && (m_sParamList.count() == anzParameter));
+}
+
+void cSCPICommand::setCmdParamList()
+{
+    const QChar* pInput = m_sCommand.data();
+    m_sCommandStr = "";
+
+    cParse Parser;
+    Parser.SetDelimiter(" :;"); // we leave a ? on the command
+
+    QString keyw;
+    do {
+        keyw = Parser.GetKeyword(&pInput); // we fetch all keywords from command
+        m_sCommandStr += keyw;
+        if (*pInput  == ':')
+            m_sCommandStr += ":";
+    } while (*pInput  == ':');
+
+    // the command is finished
+
+    Parser.SetDelimiter(";"); // we start with fetching parameters
+    Parser.SetWhiteSpace(" ");
+    m_sParamList.clear();
+
+    do {
+        keyw = Parser.GetKeyword(&pInput);
+        if (*pInput == ';') {
+            m_sParamList.append(keyw);
+            pInput++;
+        }
+    } while (*pInput != 0);
 }
--- scpi/lib/scpicommand.h
+++ scpi/lib/scpicommand.h
@@ -20,11 +20,7 @@ public:
     cSCPICommand();
     cSCPICommand(const QString& str);
     cSCPICommand(const cSCPICommand &other);
-    ~cSCPICommand();
     cSCPICommand& operator = (const QString& str);
-    /**
-      @b Sets the scpicommand from a QTextStream and returns a reference to QTextStream.
-      */
     friend QTextStream& operator >> (QTextStream& ts, cSCPICommand& cmd);
     /**
       @b Returns the whole scpicommand.
@@ -34,13 +30,7 @@ public:
       @b Only returns the command part of the scpicommand.
       */
     const QString getCommandStr() const;
-    /**
-      @b Returns the number of parameters in the command.
-      */
     quint32 getParamCount() const;
-    /**
-      @b Returns the parameter from pos.
-      */
     QString getParam(qint32 pos) const;
     /**
       @b Returns the whole parameter text after the command without last ;
@@ -48,26 +38,15 @@ public:
     QString getParam() const;
 
     const QStringList &getParamList() const;
-    /**
-      @b Returns true if command is a pure query without additional parameter.
-      */
     bool isQuery() const;
-    /**
-      @b Returns true if command is a query with (anzParameter) parameter.
-      */
     bool isQuery(quint8 anzParameter) const;
-    /**
-      @b Returns true if command is a command with (anzParameter) parameter.
-      */
     bool isCommand(quint8 anzParameter) const;
 
 private:
-    /**
-      @b D'pointer to the private library internal structure
-
-      this is used to hide the internal structure, and thus make the library ABI safe
-      */
-    cSCPICommandPrivate *d_ptr;
+    void setCmdParamList();
+    QString m_sCommand;
+    QString m_sCommandStr;
+    QStringList m_sParamList;
 };
 
 #endif // SCPICOMMAND_H
