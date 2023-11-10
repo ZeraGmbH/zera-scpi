e562cf868b6d7ba2d7021935fdcdb12948a0dd15
Andreas Müller <schnitzeltony@gmail.com>
2023-11-10 21:02:06 +0100

    scpi.h: Remove unused error definition: errcon
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- scpi/lib/scpi.h
+++ scpi/lib/scpi.h
@@ -29,13 +29,12 @@
 
 namespace SCPI
 {
-enum eSCPICmdResult { ack, nak, busy, errcon, erraut, errval, errxml,
+enum eSCPICmdResult { ack, nak, busy, erraut, errval, errxml,
                       errpath, errexec, errtimo, last };
 
 const QString scpiAnswer[errtimo+1] = { "ack",     ///< acknowledge (command recognized and executed)
                                         "nak",     ///< not acknowledge (kommando not recognized)
                                         "busy",    ///< device busy
-                                        "errcon",  ///< error connection (command recognized but no connection to device)
                                         "erraut",  ///< error authorization (command recognized but client is not auth. )
                                         "errval",  ///< error value (command recognized but incorrect parameter)
                                         "errxml",  ///< error xml (used xml file was incorrect)
