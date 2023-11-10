d8448fb5614a2c0a04b0a074178208520c0e78d2
Andreas Müller <schnitzeltony@gmail.com>
2023-11-10 23:13:34 +0100

    Remove ZSCPI response definitions
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- scpi/lib/scpi.h
+++ scpi/lib/scpi.h
@@ -29,19 +29,6 @@
 
 namespace SCPI
 {
-enum eSCPICmdResult { ack, nak, busy, erraut, errval, errxml,
-                      errpath, errexec,
-                      resultcount };
-
-const QString scpiAnswer[resultcount] = { "ack",     ///< acknowledge (command recognized and executed)
-                                          "nak",     ///< not acknowledge (kommando not recognized)
-                                          "busy",    ///< device busy
-                                          "erraut",  ///< error authorization (command recognized but client is not auth. )
-                                          "errval",  ///< error value (command recognized but incorrect parameter)
-                                          "errxml",  ///< error xml (used xml file was incorrect)
-                                          "errpath", ///< error path
-                                          "errexec", ///< error execution
-                                      };
 enum eSCPINodeType { isNode = 1, isQuery = 2, isCmd = 4, isCmdwP = 8, isXMLCmd = 16};
 enum eSCPINode { Node, Query, Cmd, CmdwP };
 }
